aa26de0734cc3afd34bec5a4550a8284bbd521aa
Barbara Johnson <johnson.barbara@gmail.com>
2019-05-14 03:33:36 +0000

    Automatically backed up by Learn

--- db/migrate/20190514033108_create_movies.rb
+++ db/migrate/20190514033108_create_movies.rb
@@ -1,10 +1,10 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
     add_column :movies, :title, :string
-    add_column :release_date, :title, :string
-    add_column :movies, :title, :string
-    add_column :movies, :title, :string
-     add_column :movies, :title, :string
+    add_column :movies, :release_date, :integer
+    add_column :movies, :director, :string
+    add_column :movies, :lead, :string
+     add_column :movies, :in_theaters, :boolean
  
 
   end

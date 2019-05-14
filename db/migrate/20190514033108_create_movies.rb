400a568b666eb172adc2960016a26c209000c1fc
Barbara Johnson <johnson.barbara@gmail.com>
2019-05-14 03:33:10 +0000

    Automatically backed up by Learn

--- db/migrate/20190514033108_create_movies.rb
+++ db/migrate/20190514033108_create_movies.rb
@@ -1,6 +1,11 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
-    add_column :artists, :favorite_food, :string
+    add_column :movies, :title, :string
+    add_column :release_date, :title, :string
+    add_column :movies, :title, :string
+    add_column :movies, :title, :string
+     add_column :movies, :title, :string
+ 
 
   end
 end

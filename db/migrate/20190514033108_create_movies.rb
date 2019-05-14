537810d2625f254e159f346ecbf4be144816dfa6
Barbara Johnson <johnson.barbara@gmail.com>
2019-05-14 03:32:24 +0000

    Automatically backed up by Learn

--- db/migrate/20190514033108_create_movies.rb
+++ db/migrate/20190514033108_create_movies.rb
@@ -1,10 +1,6 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
-       create_table :artists do |t|
-      t.string :name
-      t.string :genre
-      t.integer :age
-      t.string :hometown
-    end
+    add_column :artists, :favorite_food, :string
+
   end
 end
